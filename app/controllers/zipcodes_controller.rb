d61ac51361a2692b75bd5af9b807e4d98f9b4c9e
iamtamtamiam <vutam_a@hotmail.com>
2021-02-22 22:39:43 -0600

    add params to zipcodes controller

--- app/controllers/zipcodes_controller.rb
+++ app/controllers/zipcodes_controller.rb
@@ -1,7 +1,14 @@
 class ZipcodesController < ApplicationController
     def show
         api_key = ENV["zipwise_api_key"]
-        zips = JSON.parse( RestClient.get("https://www.zipwise.com/webservices/radius.php?key=#{api_key}&zip=90210&radius=2&format=json") )
+
+        zip_to_search = params[:zipcode]
+        raidus_to_search = params[:radius]
+
+        zips = JSON.parse( RestClient.get("https://www.zipwise.com/webservices/radius.php?key=#{api_key}&zip=#{zip_to_search}&radius=#{raidus_to_search}&format=json") )
+        #zips = JSON.parse( RestClient.get("https://www.zipwise.com/webservices/radius.php?key=#{api_key}&zip=90210&radius=2&format=json") )
+
+        #need listing = Listing.find_by(id: params[:id])
 
         if zips
             render json: zips
@@ -9,7 +16,11 @@ class ZipcodesController < ApplicationController
             render json: {message: 'zip not found'}
         end 
 
-
     end
+
+    #check the permit names
+    def zipcodes_params
+        params.require(:zipcode).permit(:zipcode, :radius)
+    end 
 
 end
